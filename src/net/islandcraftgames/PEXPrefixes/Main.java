d3b4776683906dceeb251137f268f4fbc81a0171
Rexcantor <rexcantor64@gmail.com>
2016-03-23 18:06:12 +0000

    PEXprefixes 1.1 update

--- src/net/islandcraftgames/PEXPrefixes/Main.java
+++ src/net/islandcraftgames/PEXPrefixes/Main.java
@@ -2,9 +2,12 @@ package net.islandcraftgames.PEXPrefixes;
 
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import ru.tehkode.permissions.PermissionUser;
@@ -15,20 +18,7 @@ public class Main extends JavaPlugin implements Listener {
 
 	@Override
 	public void onEnable() {
-		if (Bukkit.getOnlinePlayers().size() != 0) {
-			for (Player p : Bukkit.getOnlinePlayers()) {
-				PermissionUser pu = PermissionsEx.getUser(p);
-
-				String prefix = pu.getPrefix();
-				if (prefix == null)
-					prefix = "";
-				String suffix = pu.getSuffix();
-				if (suffix == null)
-					suffix = "";
-				p.setDisplayName(ChatColor.translateAlternateColorCodes('&',
-						prefix + p.getName() + suffix + ChatColor.RESET));
-			}
-		}
+		PEXUpdate();
 		Bukkit.getPluginManager().registerEvents(this, this);
 	}
 
@@ -38,6 +28,15 @@ public class Main extends JavaPlugin implements Listener {
 
 	@EventHandler
 	public void onPEXUpdate(PermissionEntityEvent e) {
+		PEXUpdate();
+	}
+
+	@EventHandler
+	public void onJoin(PlayerJoinEvent e) {
+		PEXUpdate();
+	}
+
+	public void PEXUpdate() {
 		if (Bukkit.getOnlinePlayers().size() != 0) {
 			for (Player p : Bukkit.getOnlinePlayers()) {
 				PermissionUser pu = PermissionsEx.getUser(p);
@@ -52,6 +51,18 @@ public class Main extends JavaPlugin implements Listener {
 						prefix + p.getName() + suffix + ChatColor.RESET));
 			}
 		}
+	}
+
+	@Override
+	public boolean onCommand(CommandSender sender, Command command,
+			String label, String[] args) {
+		if (sender.hasPermission("pexprefixes.update")) {
+			PEXUpdate();
+			sender.sendMessage(ChatColor.GREEN + "Prefixes and suffixes reloaded!");
+			return true;
+		}
+		sender.sendMessage(ChatColor.RED + "No permission.");
+		return false;
 	}
 
 }
